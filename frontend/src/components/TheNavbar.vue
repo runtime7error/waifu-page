ea6489583bd05839b3278eda4cbb079303f8994c
runtime7error <lucaslh6923@gmail.com>
2021-08-30 19:06:39 -0300

    -Add github autor profile

--- frontend/src/components/TheNavbar.vue
+++ frontend/src/components/TheNavbar.vue
@@ -17,7 +17,7 @@
     >
       <h1 class="font-bold text-4xl tracking-tight">Waifu List</h1>
       <div class="flex gap-2">
-        <a href="/">Authors</a>
+        <a href="https://github.com/runtime7error" target="_blank">Authors</a>
         <a href="https://github.com/runtime7error/waifu-page" target="_blank"
           >GitHub</a
         >
