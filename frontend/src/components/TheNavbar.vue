ff0a070dd157cdabb68cdeba65f3ec17a569c28f
runtime7error <lucaslh6923@gmail.com>
2021-08-30 19:05:10 -0300

    -Adding github project page

--- frontend/src/components/TheNavbar.vue
+++ frontend/src/components/TheNavbar.vue
@@ -18,7 +18,9 @@
       <h1 class="font-bold text-4xl tracking-tight">Waifu List</h1>
       <div class="flex gap-2">
         <a href="/">Authors</a>
-        <a href="/">GitHub</a>
+        <a href="https://github.com/runtime7error/waifu-page" target="_blank"
+          >GitHub</a
+        >
       </div>
     </div>
   </nav>
